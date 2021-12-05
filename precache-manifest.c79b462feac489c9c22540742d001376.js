45dcb72cc5b521082c500b480048a7223a629d88
Victor Dorado <vdorado@hedyla.com>
2021-12-05 01:38:18 +0100

    Updates

---
+++ precache-manifest.c79b462feac489c9c22540742d001376.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1b8995d9690b37e964cc264ad28791",
+    "url": "/victordf.com/index.html"
+  },
+  {
+    "revision": "d92a19bd54c9523c7995",
+    "url": "/victordf.com/static/css/main.2baa95f7.chunk.css"
+  },
+  {
+    "revision": "f3dbc2cb137adbf04a66",
+    "url": "/victordf.com/static/js/2.f5148b59.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/victordf.com/static/js/2.f5148b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d92a19bd54c9523c7995",
+    "url": "/victordf.com/static/js/main.1dbeec19.chunk.js"
+  },
+  {
+    "revision": "4d30d653a5decf677a8c",
+    "url": "/victordf.com/static/js/runtime-main.4754ef90.js"
+  },
+  {
+    "revision": "ec0863a5969674308b9f89bed362165e",
+    "url": "/victordf.com/static/media/acl.ec0863a5.svg"
+  },
+  {
+    "revision": "fa95bb9fab351f82d6a5d9bf58ff7151",
+    "url": "/victordf.com/static/media/agh.fa95bb9f.svg"
+  },
+  {
+    "revision": "97d20dfd192a64ef2705e4d089e2a3a1",
+    "url": "/victordf.com/static/media/android.97d20dfd.svg"
+  },
+  {
+    "revision": "c0d59b7fa428fa03cfe5bb89ce08c016",
+    "url": "/victordf.com/static/media/anpane.c0d59b7f.svg"
+  },
+  {
+    "revision": "e0f226f11185eb81a653f790b89bc8c5",
+    "url": "/victordf.com/static/media/astudio.e0f226f1.svg"
+  },
+  {
+    "revision": "24ce7d59c20fa3377259c9737c33c00d",
+    "url": "/victordf.com/static/media/aws.24ce7d59.svg"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/victordf.com/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/victordf.com/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "21af39c6435057500504c04a99b7c2fb",
+    "url": "/victordf.com/static/media/bitbucket.21af39c6.svg"
+  },
+  {
+    "revision": "6aaf8b631fb37fb89d1afe77ca97a063",
+    "url": "/victordf.com/static/media/bootstrap-4.6aaf8b63.svg"
+  },
+  {
+    "revision": "fcf006694eff05ecbd0cbe503f491dfc",
+    "url": "/victordf.com/static/media/csharp.fcf00669.svg"
+  },
+  {
+    "revision": "30260f0c75ff51b4dfddf8ac56b89438",
+    "url": "/victordf.com/static/media/docker.30260f0c.svg"
+  },
+  {
+    "revision": "8d7c09d9e93a0915edf9f1677e809f7c",
+    "url": "/victordf.com/static/media/elasticsearch.8d7c09d9.svg"
+  },
+  {
+    "revision": "270fd34e8057b6043ef7c5b81ebc7024",
+    "url": "/victordf.com/static/media/express.270fd34e.svg"
+  },
+  {
+    "revision": "563d05c8f122099899c7f858c78df121",
+    "url": "/victordf.com/static/media/fce.563d05c8.svg"
+  },
+  {
+    "revision": "f13fcb33baa610d6f4d9e581725673a3",
+    "url": "/victordf.com/static/media/firebase.f13fcb33.svg"
+  },
+  {
+    "revision": "1584fdec531cc482845492e5b70b1f3d",
+    "url": "/victordf.com/static/media/flyway.1584fdec.svg"
+  },
+  {
+    "revision": "0bbee8a6d64288d4f5c0ac96669c3617",
+    "url": "/victordf.com/static/media/gis.0bbee8a6.svg"
+  },
+  {
+    "revision": "7ca17ac8c0446bf4f9b5a2126083f851",
+    "url": "/victordf.com/static/media/github.7ca17ac8.svg"
+  },
+  {
+    "revision": "01813fae156e2e28b021999cc8c6a702",
+    "url": "/victordf.com/static/media/hedyla.01813fae.svg"
+  },
+  {
+    "revision": "0e2226211d48a71be064a7d1c1505693",
+    "url": "/victordf.com/static/media/intelij.0e222621.svg"
+  },
+  {
+    "revision": "8ad07e8bc3ab7afd831aa7955161e9a6",
+    "url": "/victordf.com/static/media/java.8ad07e8b.svg"
+  },
+  {
+    "revision": "fb33e668e269962fa7b55d7beddf81d1",
+    "url": "/victordf.com/static/media/javascript.fb33e668.svg"
+  },
+  {
+    "revision": "0b7fc752d278b5db02e03841b23ce7c5",
+    "url": "/victordf.com/static/media/jenkins.0b7fc752.svg"
+  },
+  {
+    "revision": "3a4bfd2a9f89336d57d40658eb32501d",
+    "url": "/victordf.com/static/media/jira.3a4bfd2a.svg"
+  },
+  {
+    "revision": "9af8b24311d2174d918fe786230a430d",
+    "url": "/victordf.com/static/media/junit.9af8b243.svg"
+  },
+  {
+    "revision": "0b0350b3091adcd88c9e7591b7191116",
+    "url": "/victordf.com/static/media/kibana.0b0350b3.svg"
+  },
+  {
+    "revision": "9e1e4453d7a419e52ee79f8aa1157295",
+    "url": "/victordf.com/static/media/logoVgoldenResized.9e1e4453.webp"
+  },
+  {
+    "revision": "9ea24aef9b8e670f7fe14b847d3fe44d",
+    "url": "/victordf.com/static/media/nodejs.9ea24aef.svg"
+  },
+  {
+    "revision": "95007a9df201b1ab167cc104fdb2edf5",
+    "url": "/victordf.com/static/media/postman.95007a9d.svg"
+  },
+  {
+    "revision": "7d8a68be82c8c38f89fa3eb606f467e4",
+    "url": "/victordf.com/static/media/profile.7d8a68be.webp"
+  },
+  {
+    "revision": "35f698a9203d147e6ce854231650c4e4",
+    "url": "/victordf.com/static/media/pwc.35f698a9.svg"
+  },
+  {
+    "revision": "d3f2a26ad20c084c9f0eebb6487aa1ac",
+    "url": "/victordf.com/static/media/python.d3f2a26a.svg"
+  },
+  {
+    "revision": "147f816fe23b0c012b3cf6c2df530c0c",
+    "url": "/victordf.com/static/media/scala.147f816f.svg"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/victordf.com/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/victordf.com/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/victordf.com/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "ea7f7ad1adbabeef4b57038c0a03d54e",
+    "url": "/victordf.com/static/media/spring.ea7f7ad1.svg"
+  },
+  {
+    "revision": "12205f7b6855a95a123a86e134ca2548",
+    "url": "/victordf.com/static/media/sql.12205f7b.svg"
+  },
+  {
+    "revision": "4c0479eaea8f0d6c82a327345845b6ca",
+    "url": "/victordf.com/static/media/terraform.4c0479ea.svg"
+  },
+  {
+    "revision": "c4484aaaac9ac7449b6fbeb38d31f035",
+    "url": "/victordf.com/static/media/trinity.c4484aaa.svg"
+  },
+  {
+    "revision": "c09f751b56026e57009cd7416e8efc1d",
+    "url": "/victordf.com/static/media/upc.c09f751b.svg"
+  },
+  {
+    "revision": "0e27a9d5060f4a2dbc5a52c9ed313081",
+    "url": "/victordf.com/static/media/vstudio.0e27a9d5.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dee9edfc57626e6f26cf7b0974bac67e.js"
+  "/victordf.com/precache-manifest.c79b462feac489c9c22540742d001376.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/victordf.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
